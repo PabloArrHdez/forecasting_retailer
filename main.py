44671b21ba279c2853c8502509e20f8870364917
Pablo <pabloarrhdez@gmail.com>
2025-12-23 12:42:22 +0100

    modified:   main.py

--- main.py
+++ main.py
@@ -17,51 +17,49 @@ import src.modelo as mdl
 import src.transformación as trs
 import src.visualizacion as vsl
 
+if __name__== "__main__":
+    ventas_data = r"D:\forecasting_retailer\data\raw\training\ventas.csv"
+    competencia_data = r"D:\forecasting_retailer\data\raw\training\competencia.csv"
+    inferencia_data = r"D:\forecasting_retailer\data\raw\inference\ventas_2025_inferencia.csv"
+    df_data = r"D:\forecasting_retailer\data\processed\df.csv"
+    ventas_df = pd.read_csv(ventas_data)
+    competencia_df = pd.read_csv(competencia_data)
+    inferencia_df = pd.read_csv(inferencia_data)
+    df = pd.read_csv(df_data)
 
-ventas_data = r"D:\forecasting_retailer\data\raw\training\ventas.csv"
-competencia_data = r"D:\forecasting_retailer\data\raw\training\competencia.csv"
-inferencia_data = r"D:\forecasting_retailer\data\raw\inference\ventas_2025_inferencia.csv"
-df_data = r"D:\forecasting_retailer\data\processed\df.csv"
-ventas_df = pd.read_csv(ventas_data)
-competencia_df = pd.read_csv(competencia_data)
-inferencia_df = pd.read_csv(inferencia_data)
-df = pd.read_csv(df_data)
+    ext.validacion_datos (ventas_df, competencia_df)
+    ext.nulos (ventas_df, competencia_df)
+    ext.duplicados (ventas_df, competencia_df)
+    ext.descriptivo (ventas_df, competencia_df)
+    ext.resumen_final (ventas_df, competencia_df)
 
-ext.validacion_datos (ventas_df, competencia_df)
-ext.nulos (ventas_df, competencia_df)
-ext.duplicados (ventas_df, competencia_df)
-ext.descriptivo (ventas_df, competencia_df)
-ext.resumen_final (ventas_df, competencia_df)
+    trs.fecha (ventas_df,competencia_df)
+    trs.union (ventas_df, competencia_df)
+    trs.es_black_friday(fecha)
+    trs.es_cyber_monday(fecha)
+    trs.columnnas(df)
+    trs.lags(df)
+    trs.descuento(df)
+    trs.eliminacion_columnas(df)
+    trs.nuevas_columnas(df)
+    trs.dumies(df)
+    trs.fecha_inferencia(inferencia_df)
+    trs.festivo_inferencia(inferencia_df)
+    trs.es_black_friday_inferencia(inferencia_df)
+    trs.es_cyber_monday_inferencia(inferencia_df)
+    trs.columna_inferencia(inferencia_df)
+    trs.lags_inferencia(inferencia_df)
+    trs.nuevas_columnas_inferencia(inferencia_df)
 
-trs.fecha (ventas_df,competencia_df)
-trs.union (ventas_df, competencia_df)
-trs.es_black_friday(fecha)
-trs.es_cyber_monday(fecha)
-trs.columnnas(df)
-trs.lags(df)
-trs.descuento(df)
-trs.eliminacion_columnas(df)
-trs.nuevas_columnas(df)
-trs.dumies(df)
-trs.fecha_inferencia(inferencia_df)
-trs.festivo_inferencia(inferencia_df)
-trs.es_black_friday_inferencia(inferencia_df)
-trs.es_cyber_monday_inferencia(inferencia_df)
-trs.columna_inferencia(inferencia_df)
-trs.lags_inferencia(inferencia_df)
-trs.nuevas_columnas_inferencia(inferencia_df)
+    vsl.unidades_vendidas_año (df)
+    vsl.unidades_vendidas_semana (df)
+    vsl.unidades_vendidas_categoria (df)
+    vsl.unidades_vendidas_subcat (df)
+    vsl.top_productos (df)
+    vsl.densidad_precios (df)
+    vsl.prediccion_realidad (df)
 
-vsl.unidades_vendidas_año (df)
-vsl.unidades_vendidas_semana (df)
-vsl.unidades_vendidas_categoria (df)
-vsl.unidades_vendidas_subcat (df)
-vsl.top_productos (df)
-vsl.densidad_precios (df)
-vsl.prediccion_realidad (df)
-
-
-mdl.entrenamiento (df)
-mdl.train_validation (df)
-mdl.estimador (df)
-mdl.modelo_train (df)
-mdl.
+    mdl.entrenamiento (df)
+    mdl.train_validation (df)
+    mdl.estimador (df)
+    mdl.modelo_train (df)

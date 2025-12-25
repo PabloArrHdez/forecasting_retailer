4ec49d85b29b1363c5196da2a781a74bbdca2aec
Pablo <pabloarrhdez@gmail.com>
2025-12-25 17:15:09 +0100

    modified:   app/pages/_1_-Exploracion.py

--- app/pages/_1_-Exploracion.py
+++ app/pages/_1_-Exploracion.py
@@ -50,10 +50,7 @@ st.markdown(
 st.title ("Explorador de artículos deportivos (2021-2024)​.")
 
 ventas_data = r"D:\forecasting_retailer\data\raw\training\ventas.csv"
-competencia_data = r"D:\forecasting_retailer\data\raw\training\competencia.csv"
 
 ventas_df = pd.read_csv(ventas_data)
-competencia_df = pd.read_csv(competencia_data)
 
 st.dataframe(ventas_df)
-st.dataframe(competencia_df)

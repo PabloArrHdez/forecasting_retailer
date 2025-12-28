00660e085cd7ef63670efd9a11df09a83e0da8f4
Pablo <pabloarrhdez@gmail.com>
2025-12-28 17:12:37 +0100

    modified:   "app/pages/_2_-Predicci\303\263n.py"

--- app/pages/_2_-Predicción.py
+++ app/pages/_2_-Predicción.py
@@ -31,13 +31,13 @@ st.markdown(
     h1, h2, h3, h4, h5, h6, p, span {{
         color: white !important;
     }}
-        section[data-testid="stSidebar"] > div {{
+    section[data-testid="stSidebar"] > div {{
         background-color: rgba(0, 0, 0, 0.6);
         padding: 15px;
         border-radius: 10px;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
     }}
-        /* Créditos de la imagen */
+    /* Créditos de la imagen */
     .credit {{
         position: fixed;
         bottom: 10px;
@@ -51,11 +51,18 @@ st.markdown(
         box-shadow: 0 0 6px rgba(255, 255, 255, 0.4);
         z-index: 100;
     }}
-        .credit a {{
+    .credit a {{
         color: white;
         text-decoration: underline;
     }}
-
+    /* Personalización de los widgets st.metric */
+    div[data-testid="stMetric"] {{
+        color: white !important;
+        background: transparent !important;
+    }}
+    div[data-testid="stMetric"] span {{
+        color: white !important;
+    }}
     </style>
 
     <div class="credit">
